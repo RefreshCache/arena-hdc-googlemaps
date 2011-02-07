37695d66b509857835d4e3c5006100233a2bba11
Daniel Hazelbaker <cabal95@users.noreply.github.com>
2011-02-07 18:51:59 +0000

    Added new classes: GeocodedAddress, Google, GoogleService, Maps.FamilyPlacemark, Maps.PersonPlacemark, Maps.Placemark. New GoogleService web service provides a JS interface into lazy loading of google maps information, other new classes are support classes for this feature. Added new PeopleInRange UserControl (currently empty) to provide a simple user control to see the people that are in range of an address.

---
+++ UserControls/PeopleInRange.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="PeopleInRange.ascx.cs" CodeBehind="PeopleInRange.ascx.cs" Inherits="ArenaWeb.UserControls.Custom.HDC.GoogleMaps.PeopleInRange" %>
---
+++ UserControls/PeopleInRange.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ArenaWeb.UserControls.Custom.HDC.GoogleMaps
+{
+    public partial class PeopleInRange : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
