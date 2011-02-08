9ba2b7b8195a2a74a78eb5803f93c1d351c91abb
cabal95 <cabal95@5eabe0c6-04ba-4e6c-a47d-40e418f4085f>
2011-02-08 17:23:45 +0000

    Update comments on some files.

--- trunk/Library/GeocodedAddress.cs
+++ trunk/Library/GeocodedAddress.cs
@@ -5,11 +5,25 @@ using System.Text;
 
 namespace Arena.Custom.HDC.GoogleMaps
 {
+    /// <summary>
+    /// Generic holder class that defines the Latitude and Longitude of an address.
+    /// </summary>
     public class GeocodedAddress
     {
-        public Double Latitude, Longitude;
+        /// <summary>
+        /// The latitude of the address.
+        /// </summary>
+        public Double Latitude;
+
+        /// <summary>
+        /// The longitude of the address.
+        /// </summary>
+        public Double Longitude;
 
 
+        /// <summary>
+        /// Generic constructor.
+        /// </summary>
         public GeocodedAddress()
         {
         }
--- trunk/Library/RadiusLoader.cs
+++ trunk/Library/RadiusLoader.cs
@@ -5,14 +5,45 @@ using System.Text;
 
 namespace Arena.Custom.HDC.GoogleMaps
 {
+    /// <summary>
+    /// This class defines a group of people that will be loaded and placed on a
+    /// GoogleMap when the page is rendered. It uses a center point and distance
+    /// from that point to determine which people to place on the map.
+    /// </summary>
     [Serializable]
     public class RadiusLoader
     {
-        public Double Latitude, Longitude;
+        #region Properties
+
+        /// <summary>
+        /// The latitude of the center point to use.
+        /// </summary>
+        public Double Latitude;
+
+        /// <summary>
+        /// The longitude of the center point to use.
+        /// </summary>
+        public Double Longitude;
+
+        /// <summary>
+        /// The distance, in miles, from the center point that people should be
+        /// loaded.
+        /// </summary>
         public Double Distance;
+
+        /// <summary>
+        /// The type of things to load.
+        /// </summary>
         public RadiusLoaderType LoaderType;
 
+        #endregion
 
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor, create an empty loader.
+        /// </summary>
         public RadiusLoader()
         {
             this.Latitude = 0;
@@ -20,12 +51,24 @@ namespace Arena.Custom.HDC.GoogleMaps
             this.Distance = 0;
             this.LoaderType = RadiusLoaderType.Individuals;
         }
+
+        #endregion
     }
 
 
+    /// <summary>
+    /// Defines the type of objects that will be loaded in a RadiusLoader.
+    /// </summary>
     public enum RadiusLoaderType
     {
+        /// <summary>
+        /// Load individual people records onto the map.
+        /// </summary>
         Individuals = 0,
+
+        /// <summary>
+        /// Loads entire family units onto the map.
+        /// </summary>
         Families = 1
     }
 }
